2df3fd899fe7e7bf3a43dcd777448611494d9048
relinkz <seb.lundgren@gmail.com>
2017-09-02 16:22:41 +0200

    ADD blurr the second pixel in array, should be easy to transfer into loop

--- copyImage.asm
+++ copyImage.asm
@@ -41,7 +41,7 @@ writeToFile:
 	;3. Put the pointer to data in ECX
 	;4. Put buffer size of data in EDX
 	;5. call kernel
-	
+
 	;returns the actual nr of bytes written in EAX
 	
 	;mov ecx, [info + 2]
@@ -70,7 +70,7 @@ writeToFile:
 	mov eax, 4
 	mov ebx, [fd_out]
 	mov ecx, blurr
-	mov edx, 2
+	mov edx, 6
 	int 0x80
 
 	ret
@@ -78,6 +78,8 @@ writeToFile:
 blurrTopRow:
 	;from 0 - 251 there will be no pixels above the active pixel
 
+	;[x][o]
+	;[o][o]
 	mov al, byte [info + 0] 	 ;add the pixel itself
 	movzx ax, al 		         ;convert byte to word
 	
@@ -101,11 +103,89 @@ blurrTopRow:
 	mov ax, [sum] 				 ;prep sum for divition
 	movzx eax, ax 				 ;convert word to full 32-bit register
 
-	mov ebp, 7 	 				 ;Divition by 7
+	mov ebp, 4 	 				 ;Divition by 7
 	mov edx, 0 					 ;how many rest products that are stored
 	div ebp 					 ;call divition
 
 	mov [blurr + 0], al 		 ;save value to blurr
+
+	;[o][x][o] r1
+	;[o][o][o] r2
+
+	mov ecx, 0
+
+	;loop here
+
+	;reset sum
+	mov [sum], word 0
+
+	inc ecx 						;ecx is iterator
+	
+	;first row (r1)
+
+	mov al, byte [info + ecx]		;read pixel i, pixel to blurr
+	movzx ax, al 					;convert byte to word
+
+	mov [sum], ax 					;store into sum
+
+	mov al, byte [info + ecx - 1]	;get pixel i-1
+	movzx ax, al 					;convert byte to word
+
+	add [sum], ax 					;add to sum
+
+	mov al, byte [info + ecx + 1] 	;get pixel i + 1
+	movzx ax, al 					;convert byte to word
+
+	add [sum], ax 					;add to sum
+
+	;Done with the first row
+	;second row (r2)
+
+	mov al, byte [info + ecx + 250] ;get pixel i+ 250
+	movzx ax, al 					;convert byte to word
+
+	add [sum], ax 					;add to sum
+
+	mov al, byte [info + ecx + 251] ;get pixel i+ 251
+	movzx ax, al 					;convert byte to word
+
+	add [sum], ax 					;add to sum
+
+	mov al, byte [info + ecx + 252] ;get pixel i + 252
+	movzx ax, al 					;convert byte to word
+
+	add [sum], ax
+
+	mov ax, [sum] 				 ;prep sum for divition
+	movzx eax, ax 				 ;convert word to full 32-bit register
+
+	mov ebp, 6 	 				 ;Divition by 7
+	mov edx, 0 					 ;how many rest products that are stored
+	div ebp 					 ;call divition
+
+	;second pixel is correctly blurred
+	;next up is to do a jump condition that does this to
+	;all 250 times, for all upper rows
+
+	mov [blurr + ecx], al
+
+	;mov al, byte [info + 1 - 1]
+	;mov [blurr + 0], al
+
+	;mov al, byte [info + 1]
+	;mov [blurr + 1], al
+
+	;mov al, byte [info + 1 + 1]
+	;mov [blurr + 2], al
+
+	;mov al, byte [info + 1 + 250]
+	;mov [blurr + 3], al
+
+	;mov al, byte [info + 1 + 251]
+	;mov [blurr + 4], al
+
+	;mov al, byte [info + 1 + 252]
+	;mov [blurr + 5], al
 
 	ret
 
@@ -204,3 +284,4 @@ fd_in resb 1
 info resb 64256
 blurr resb 64256
 sum resw 1
+activePixel resw 1
